27afcc2736ae3f3f2a3466466188236170cf674e
Szesny32 <bartoszszczesny.studia@gmail.com>
2022-10-29 15:39:41 +0200

    AuthController - Added register function

--- backend-laravel/routes/api.php
+++ backend-laravel/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::post('/register', [AuthController::class, 'register']);
+
+
+//TEST
+Route::get('/users', [AuthController::class, 'test_index']);
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
